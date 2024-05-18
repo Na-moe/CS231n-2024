e4adfb425176f4570a24e00cd9da01ae175e6616
yangziming <yangziming@buaa.edu.cn>
2024-05-18 23:12:22 +0800

    complete SSL notebook

--- assignment3/cs231n/simclr/utils.py
+++ assignment3/cs231n/simclr/utils.py
@@ -37,6 +37,9 @@ def train(model, data_loader, train_optimizer, epoch, epochs, batch_size=32, tem
         # Then compute the loss using simclr_loss_vectorized.                        #
         ##############################################################################
         
+        _, out_left = model(x_i)
+        _, out_right = model(x_j)
+        loss = simclr_loss_vectorized(out_left, out_right, temperature, device)
         
         ##############################################################################
         #                               END OF YOUR CODE                             #
